fbc5f5b4803d44a1873a72e9b38b1acfa8ebb43f
Jakub Duchon <jakub.duchon9@gmail.com>
2019-11-28 17:45:35 +0100

    Adding default values for SensorDataTypes enum

--- src/main/java/model/SensorDataTypes.java
+++ src/main/java/model/SensorDataTypes.java
@@ -1,8 +1,18 @@
 package model;
 
 public enum SensorDataTypes {
-    CO2,
-    HUM,
-    TEMP,
-    LIGHT
+    CO2("CO2"),
+    HUM("Humidity"),
+    TEMP("Temperature"),
+    LIGHT("Light");
+
+    private String value;
+
+    SensorDataTypes(final String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
