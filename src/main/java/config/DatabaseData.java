46d73b29374ee3e4eb6a7fd7baf9ffd76d3ea669
Jakub Duchon <jakub.duchon9@gmail.com>
2019-11-28 18:16:01 +0100

    Made server_name and port_number private in DatabaseData class

--- src/main/java/config/DatabaseData.java
+++ src/main/java/config/DatabaseData.java
@@ -2,10 +2,9 @@ package config;
 
 public class DatabaseData {
 	public static final String DB_NAME = "SEP4_PMI";
-    public static final String SERVER_NAME = "192.168.87.193\\SEP4_PMI";
-    public static final int PORT_NUMBER = 1433;
+    private static final String SERVER_NAME = "192.168.87.193\\SEP4_PMI";
+    private static final int PORT_NUMBER = 1433;
     public static final String DB_USERNAME = "SEP4_PMI";
     public static final String DB_PASSWORD = "#SEP#4_#PMI#_#!";
     public static final String MY_SQL_URL = "jdbc:sqlserver://" + SERVER_NAME + ":" + PORT_NUMBER;
-
 }
