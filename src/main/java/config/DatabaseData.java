3dc12964d881f935e872e72a19f2fbc64139b4f8
Naya Al Tahan <naya.altahan00@gmail.com>
2019-12-09 10:25:34 +0100

    Database configurations now set to the campus server

--- src/main/java/config/DatabaseData.java
+++ src/main/java/config/DatabaseData.java
@@ -3,9 +3,9 @@ package config;
 public class DatabaseData {
     public static final String DB_NAME = "SEP4_PMI";
     public static final int PORT_NUMBER = 1433;
-    public static final String DB_USERNAME = "nayaya";
-    public static final String DB_PASSWORD = "tahani1234567890@";
+    public static final String DB_USERNAME = "SEP4A19G3";
+    public static final String DB_PASSWORD = "SEP4A19G3";
     // public static final String MY_SQL_URL = "jdbc:sqlserver://" + SERVER_NAME + ":" + PORT_NUMBER;
-    public static final String MY_SQL_URL =  "jdbc:sqlserver://localhost";
+    public static final String MY_SQL_URL =  "jdbc:sqlserver://10.200.131.2";
     //jdbc:sqlserver://localhost:1433;databaseName=PMI_SEP4
 }
