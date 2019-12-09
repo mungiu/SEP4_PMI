961833e21c8e23ca265184f5344754f42c8ba5b3
Naya Al Tahan <naya.altahan00@gmail.com>
2019-12-09 11:59:28 +0100

    Update Queries.java
    Now all the queries are updated to work on the school campus

--- src/main/java/utils/Queries.java
+++ src/main/java/utils/Queries.java
@@ -14,9 +14,9 @@ public class Queries {
 			"WHERE User_ID = ?;";
 
 	public static final String GET_PLANT_DATA_BY_TYPE_AND_PLANT_ID = "select top 1 PD.Data_ID, PD.sensor_type, PD.sensor_value, PD.timestamp from SEP4_PMI.dbo.PlantData PD where PD.Sensor_Type = ? AND PD.Plant_ID = ? ORDER BY PD.TimeStamp DESC;";
-	public static final String CREATE_PLANT = "insert into dbo.Plant(Plant_ID ,Profile_ID, PlantName) values (?,?,?);";
-	public static final String UPDATE_PLANT = "update dbo.Plant set Plant_ID = ?, Profile_ID = ?, PlantName = ? where Plant_ID = ?;";
-	public static final String DELETE_PLANT = "delete from dbo.Plant where Plant_ID = ?;";
+	public static final String CREATE_PLANT = "insert into SEP4_PMI.dbo.Plant(Plant_ID ,Profile_ID, PlantName) values (?,?,?);";
+	public static final String UPDATE_PLANT = "update SEP4_PMI.dbo.Plant set Plant_ID = ?, Profile_ID = ?, PlantName = ? where Plant_ID = ?;";
+	public static final String DELETE_PLANT = "delete from SEP4_PMI.dbo..Plant where Plant_ID = ?;";
 
 	public static final String GET_PLANT_PROFILES = "select PP.Profile_Id, PP.Profile_Name, "+
 			"PP.CO2_Max, PP.CO2_Min, "+
@@ -24,19 +24,19 @@ public class Queries {
 			"PP.Tem_Max, PP.Tem_Min, "+
 			"PP.Light_Max, PP.Light_Min "+
 			"from SEP4_PMI.dbo.PlantProfile PP where user_ID = ?;";
-	public static final String CREATE_PLANT_PROFILE = "insert into dbo.PlantProfile (User_ID, Profile_Name, CO2_Max, CO2_Min, Hum_Max, Hum_Min, Tem_Max, Tem_Min, Light_Max, Light_Min) values (?,?,?,?,?,?,?,?,?,?);";
-	public static final String UPDATE_PLANT_PROFILE = "update dbo.PlantProfile set User_ID = ?, Profile_Name = ?, CO2_Max = ?, CO2_Min = ?, Hum_Max = ?, Hum_Min = ?, Tem_Max = ?, Tem_Min = ?, Light_Max = ?, Light_Min = ? where Profile_ID = ?;";
-	public static final String DELETE_PLANT_PROFILE = "delete from dbo.PlantProfile where Profile_ID = ?;";
+	public static final String CREATE_PLANT_PROFILE = "insert into SEP4_PMI.dbo.PlantProfile (User_ID, Profile_Name, CO2_Max, CO2_Min, Hum_Max, Hum_Min, Tem_Max, Tem_Min, Light_Max, Light_Min) values (?,?,?,?,?,?,?,?,?,?);";
+	public static final String UPDATE_PLANT_PROFILE = "update SEP4_PMI.dbo.PlantProfile set User_ID = ?, Profile_Name = ?, CO2_Max = ?, CO2_Min = ?, Hum_Max = ?, Hum_Min = ?, Tem_Max = ?, Tem_Min = ?, Light_Max = ?, Light_Min = ? where Profile_ID = ?;";
+	public static final String DELETE_PLANT_PROFILE = "delete from SEP4_PMI.dbo.PlantProfile where Profile_ID = ?;";
 
-	public static final String GET_USER = "select * from dbo.Users where User_ID = ?;";
-	public static final String CREATE_USER = "insert into dbo.Users(User_ID, Password) values (?,?);";
-	public static final String UPDATE_USER = "update dbo.Users set User_ID = ?, Password = ? where User_ID = ?;";
-	public static final String DELETE_USER = "delete from dbo.Users where User_ID = ?;";
+	public static final String GET_USER = "select * from SEP4_PMI.dbo.Users where User_ID = ?;";
+	public static final String CREATE_USER = "insert into SEP4_PMI.dbo.Users(User_ID, Password) values (?,?);";
+	public static final String UPDATE_USER = "update SEP4_PMI.dbo.Users set User_ID = ?, Password = ? where User_ID = ?;";
+	public static final String DELETE_USER = "delete from SEP4_PMI.dbo.Users where User_ID = ?;";
 
-	public static final String GET_PLANTINFO = "select * from dbo.PlantInfo where ID = ?;";
-	public static final String CREATE_PLANTINFO = "insert into dbo.PlantInfo(Plant_ID, Sensor_Type, Sensor_Value, TimeStamp) values (?,?,?,?);";/*CURRENT_TIMESTAMP instead of the last ? If you want the stamp from the Microsoft SQL server. Else retreive before on java server or device.*/
-	public static final String UPDATE_PLANTINFO = "update dbo.PlantInfo set ID = ?, Plant_ID = ?, Sensor_Type  = ?, Sensor_Value = ?, TimeStamp = ? where Plant_ID = ?;";
-	public static final String DELETE_PLANTINFO = "delete from dbo.PlantInfo where ID = ?;";
-	public static final String GET_PLANT_BY_ID = "select * from dbo.PlantProfile where Profile_ID = ?;";
+	public static final String GET_PLANTINFO = "select * from SEP4_PMI.dbo.PlantInfo where ID = ?;";
+	public static final String CREATE_PLANTINFO = "insert into SEP4_PMI.dbo.PlantInfo(Plant_ID, Sensor_Type, Sensor_Value, TimeStamp) values (?,?,?,?);";/*CURRENT_TIMESTAMP instead of the last ? If you want the stamp from the Microsoft SQL server. Else retreive before on java server or device.*/
+	public static final String UPDATE_PLANTINFO = "update SEP4_PMI.dbo.PlantInfo set ID = ?, Plant_ID = ?, Sensor_Type  = ?, Sensor_Value = ?, TimeStamp = ? where Plant_ID = ?;";
+	public static final String DELETE_PLANTINFO = "delete from SEP4_PMI.dbo.PlantInfo where ID = ?;";
+	public static final String GET_PLANT_BY_ID = "select * from SEP4_PMI.dbo.PlantProfile where Profile_ID = ?;";
 
 }
