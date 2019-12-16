70bc17c1e42a7e3bb1cdf04bea928198c8141076
Naya Al Tahan <naya.altahan00@gmail.com>
2019-12-17 00:25:18 +0100

    PlantDataDao definition

--- src/main/java/config/DatabaseData.java
+++ src/main/java/config/DatabaseData.java
@@ -1,11 +1,7 @@
 package config;
 
 public class DatabaseData {
-    public static final String DB_NAME = "SEP4_PMI";
-    public static final int PORT_NUMBER = 1433;
     public static final String DB_USERNAME = "SEP4A19G3";
     public static final String DB_PASSWORD = "SEP4A19G3";
-    // public static final String MY_SQL_URL = "jdbc:sqlserver://" + SERVER_NAME + ":" + PORT_NUMBER;
     public static final String MY_SQL_URL =  "jdbc:sqlserver://10.200.131.2";
-    //jdbc:sqlserver://localhost:1433;databaseName=PMI_SEP4
 }
