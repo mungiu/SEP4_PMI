62a0ad97770614aa81fe88383a831aa403d85e2f
Maria4lexzy <37138736+Maria4lexzy@users.noreply.github.com>
2019-12-16 14:42:09 +0100

    initialization

---
+++ src/main/java/gateway/Gateway.java
@@ -0,0 +1,10 @@
+package gateway;
+
+import java.net.http.WebSocket;
+
+public class Gateway {
+    public static void main(String[] args) {
+        LoRaClient loRaClient = new LoRaClient();
+
+    }
+}
